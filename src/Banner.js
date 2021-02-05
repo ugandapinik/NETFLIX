34a3bef7d1797f4fc480d37f3c7a7787ec9745e5
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-05 18:11:36 -0330

    Banner: description

--- src/Banner.js
+++ src/Banner.js
@@ -50,15 +50,7 @@ function Banner() {
                     <button className="banner__button">My List</button>
                 </div>
 
-                <h1 className="banner__description">{ truncate(`This is a test description This is a test description 
-                This is a test description 
-                This is a test description 
-                This is a test description 
-                This is a test description 
-                This is a test description 
-                This is a test description 
-                This is a test description 
-                This is a test description`, 150) }</h1>
+                <h1 className="banner__description">{ truncate(movie?.overview, 150) }</h1>
             </div>
 
             <div className="banner--fadeBottom"></div>
