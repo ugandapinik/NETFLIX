514eaf605de5271df643c1eb015a308a38e43ac4
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-06 16:07:43 -0330

    Navbar included

---
+++ src/HomeScreen.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav'
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            {/* Navbar */}
+            <Nav />
+
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+
+            {/* Banner */}
+
+            {/* Rows */}
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ src/Nav.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react'
+import "./Nav.css"
+
+function Nav() {
+
+    const [show, setShow] = useState(false)
+
+    const transitionNavBar = () => {
+        if(window.scrollY > 100){
+            setShow(true)
+        }else{
+            setShow(false)
+        }
+    }
+
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar)
+        return () => window.removeEventListener("scroll", transitionNavBar)
+    }, [])
+
+
+
+    return (
+        <div className="nav nav__black">
+            <div className="nav_contents">
+                <img 
+                    className="nav__logo"
+                    src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="Netflix Logo" />
+
+
+                <img
+                    className="nav__avatar"
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                    alt="Netflix Avatar" />
+            </div>
+        </div>
+    )
+}
+
+export default Nav
