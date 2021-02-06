52387d0e938f493cf43b3497f13d2b4d7301572c
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-06 15:09:00 -0330

    SETUP: COMPLETE

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC2PLyUwNPxVSq0TUCUOiLVn3WfweLPH5Y",
+    authDomain: "netflix-2e4d7.firebaseapp.com",
+    projectId: "netflix-2e4d7",
+    storageBucket: "netflix-2e4d7.appspot.com",
+    messagingSenderId: "834211806154",
+    appId: "1:834211806154:web:5e3c49d82b0494a63dad33"
+  };
