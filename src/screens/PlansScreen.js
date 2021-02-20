c2b657d565edbc8b5987b5507957d139ebe39bc5
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-19 22:23:17 -0330

    Netflix: plan updated from firestore

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -8,23 +8,49 @@ function PlansScreen() {
 
     // fetching product information from firebase
     useEffect(() => {
-        db.collection('products')
+        db.collection('products').where("active", "==", true)
+        .get()
+        .then((querySnapshot) => {
+            const products = {}
+
+            querySnapshot.forEach(async productDoc => {
+                products[productDoc.id] = productDoc.data()
+                const priceSnap = await productDoc.ref.collection("prices").get();
+
+                priceSnap.docs.forEach(price => {
+                    products[productDoc.id].prices = {
+                        priceId: price.id,
+                        priceData: price.data()
+                    }
+                })
+            })
+            
+            setProducts(products)
+        })
+
     }, [])
 
 
+    console.log(products)
+
 
     return (
-        <div>
-            <div className="PlansScreen__plan">
-                <div className="planScreen__info">
-                    <h5>Annual</h5>
-                    <h6>Monthly</h6>
-                </div>
-                
-                <button>
-                Subscribe
-                </button>
-            </div>
+        <div className="plansScreen">
+            { Object.entries(products).map(([productId, productData]) => {
+                // TODO add some logic to check if the user subscription is active
+
+
+                return (
+                    <div className="plansScreen__plan">
+                        <div className="plansScreen__info">
+                            <h5>{ productData.name }</h5>
+                            <h6>{ productData.description }</h6>
+                        </div>
+
+                        <button>Subscription</button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
